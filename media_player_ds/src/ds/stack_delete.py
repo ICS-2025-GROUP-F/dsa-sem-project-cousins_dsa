3c2a525a8a157ee43eaa929140bc406410d2ef59
Kathy Auma <159114766+KatthyAuma@users.noreply.github.com>
2025-07-07 14:40:51 +0300

    Removing songs using Stack

--- media_player_ds/src/ds/stack_delete.py
+++ media_player_ds/src/ds/stack_delete.py
@@ -1,11 +1,38 @@
 class DeleteStack:
     def __init__(self):
+        # This list will store deleted songs temporarily, like a recycle bin.
         self.stack = []
 
     def push_song(self, song):
-        """Push deleted song to stack"""
-        pass  # TODO
+        # Add a deleted song to the top of the stack.
+        self.stack.append(song)
+        print(f"Song '{song}' added to delete stack.")
 
     def pop_and_delete(self):
-        """Delete most recently deleted song"""
-        pass  # TODO
+        # If there are songs in the stack, remove the most recent one.
+        if self.stack:
+            deleted_song = self.stack.pop()
+            print(f"Song '{deleted_song}' permanently deleted.")
+            return deleted_song
+        else:
+            # If the stack is empty, there's nothing to delete.
+            print("Delete stack is empty. No song to delete.")
+            return None
+
+    def peek(self):
+        # Return the most recent song added to the stack without removing it.
+        if self.stack:
+            return self.stack[-1]
+        else:
+            # If the stack is empty, return None.
+            return None
+
+    def is_empty(self):
+        # Return True if the stack has no songs, False otherwise.
+        return len(self.stack) == 0
+
+    def __str__(self):
+        # Return a string showing the stack contents from top to bottom.
+        if not self.stack:
+            return "Delete stack is empty."
+        return "Delete stack (top to bottom):\n" + "\n".join(reversed(self.stack))
